feb72b45c4fe7c56d63e9c78a570110af60a6cc5
Nenad Mikša <nenad.miksa@microblink.com>
2020-09-21 18:31:15 +0200

    support building with exceptions disabled

--- cpr/timeout.cpp
+++ cpr/timeout.cpp
@@ -11,6 +11,7 @@ long Timeout::Milliseconds() const {
     static_assert(std::is_same<std::chrono::milliseconds, decltype(ms)>::value,
                   "Following casting expects milliseconds.");
 
+#if __cpp_exceptions >= 199711L
     if (ms.count() > std::numeric_limits<long>::max()) {
         throw std::overflow_error(
                 "cpr::Timeout: timeout value overflow: " + std::to_string(ms.count()) + " ms.");
@@ -19,6 +20,7 @@ long Timeout::Milliseconds() const {
         throw std::underflow_error(
                 "cpr::Timeout: timeout value underflow: " + std::to_string(ms.count()) + " ms.");
     }
+#endif
 
     return static_cast<long>(ms.count());
 }
